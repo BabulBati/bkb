cb0101e7b6cbd67019c1906fa0a0f2dc03432320
BabulBati <118089674+BabulBati@users.noreply.github.com>
2023-05-08 12:29:08 +0545

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,20 +8,6 @@ function myFunction() {
   }
 }
 
-var btnContainer = document.getElementById("navbarNav");
-var btns = btnContainer.getElementsByClassName("nav-link");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
 
-    
-    if (current.length > 0) {
-      current[0].className = current[0].className.replace(" active", "");
-    }
-
-    
-    this.className += " active";
-  });
-}
 
 
